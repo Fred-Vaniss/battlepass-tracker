9ffc6fa9b887fffcee1885cf556a290fc7b1e625
Frédérick Van Isschot <46893914+Fred-Vaniss@users.noreply.github.com>
2021-02-05 11:44:01 +0100

    Set up router and base elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import React, {useState, useEffect, useRef} from 'react';
+import { BrowserRouter as Router, Route, useLocation } from 'react-router-dom'
+import './style.scss';
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Route path="/">
+        <QueryApp/>
+      </Route>
+    </Router>
   );
+}
+
+const useQuery = () => {
+  return new URLSearchParams(useLocation().search);
+}
+
+const QueryApp = () => {
+  return (
+    <>
+      <div class="container">
+        
+      </div>
+    </>
+  )
 }
 
 export default App;

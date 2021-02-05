e72b2424ae40f83582fcf70a97e003faa04a90b5
Frédérick Van Isschot <46893914+Fred-Vaniss@users.noreply.github.com>
2021-02-05 13:43:29 +0100

    Progress bar and days measure finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect, useRef} from 'react';
 import { BrowserRouter as Router, Route, useLocation } from 'react-router-dom'
-import './style.scss';
+import ProgressBar from './components/ProgressBar';
 
 function App() {
   return (
@@ -12,15 +12,59 @@ function App() {
   );
 }
 
+
+
 const useQuery = () => {
   return new URLSearchParams(useLocation().search);
 }
 
 const QueryApp = () => {
+  const [level,setLevel] = useState(0)
+
+  const setQueryString = (level) => {
+    const newUrl =  window.location.protocol + "//" +
+                    window.location.host +
+                    window.location.pathname +
+                    '?lvl='+level;
+
+    window.history.pushState({ path: newUrl }, "", newUrl);
+  }
+
+  let query = useQuery();
+
+  const firstLoad = useRef(true);
+
+  if (firstLoad.current) {
+    const queryLevel = query.get('lvl')
+    if (!queryLevel) {
+      setQueryString(level)
+    } else {
+      setLevel(query.get("lvl"))
+    }
+    firstLoad.current = false;
+  }
+
+  const handleLevelChange = e => {
+    setLevel(e.target.value)
+    setQueryString(level)
+  }
+
+  useEffect(() => {
+    setQueryString(level)
+  }, [level])
+
   return (
     <>
       <div class="container">
-        
+        <h1>Battlepass tracker</h1>
+        <div className="level-form">
+          <label htmlFor="level">Battlepass level: </label>
+          <input  type="number"
+                  name="level"
+                  value={level}
+                  onChange={handleLevelChange}/>
+        </div>
+        <ProgressBar level={level}/>
       </div>
     </>
   )
---
+++ src/components/ProgressBar.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const ProgressBar = props => {
+
+  const dates = {
+    start: new Date("02/03/2021"),
+    current: new Date(),
+    end: new Date("05/04/2021")
+  }
+
+  let timeLeft = Math.round((dates.end - dates.current) / (1000 * 3600 * 24))
+  let timeElapsed = Math.round((dates.current - dates.start) / (1000 * 3600 * 24))
+
+  let level = props.level
+
+  let levelPcnt = level/110*100
+  let daysPcnt = (timeElapsed/90*100)
+
+
+  console.log(timeElapsed)
+
+  console.log(dates)
+
+  return (
+    <div className="progress-container">
+      <span className="dates">{dates.start.toLocaleString('default', {day:"numeric", month: 'long'})}</span>
+      <div className="progress bg">
+        <div className="progress-days" style={{left: daysPcnt+"%"}}>
+          <span>{timeLeft} jours</span>
+          <div className="measure"></div>
+        </div>
+        <div className="progress bar" style={{width: levelPcnt+"%"}}></div>
+      </div>
+      <span className="dates">{dates.end.toLocaleString('default', {day:"numeric", month: 'long'})}</span>
+    </div>
+  )
+}
+
+
+export default ProgressBar;
